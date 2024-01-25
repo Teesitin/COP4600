43b4fa20fbe61512dc0a1d5d46ffafc069c91680
aterry5 <123323801+aterry5@users.noreply.github.com>
2024-01-25 12:39:49 -0500

    FIFO and Metric Calculation Added

--- PA1/scheduler-gpt.py
+++ PA1/scheduler-gpt.py
@@ -1,3 +1,5 @@
+# Amari Terry
+
 import json
 import os
 import sys
@@ -42,6 +44,82 @@ def parse_file(file_name):
 
     return data
 
+
+def fifo_scheduler(num_processes, process_names, total_cpu_time, arrival_time, burst_time):
+    process_queue = []
+    start_time = [0] * num_processes
+    finish_time = [0] * num_processes
+    wait_time = [0] * num_processes
+    turnaround_time = [0] * num_processes
+    response_time = [0] * num_processes
+    idle_times = []
+
+    current_time = 0
+    notRunning = True
+    current_process = 0
+
+    with open("output.txt", "w") as output_file:
+     
+        # Manually created
+        output_file.write(str(num_processes).rjust(3)+" processes\n")
+        output_file.write("Using First-Come First-Served\n")
+
+        # ChatGPT created but manually reformatted and rearranged
+        while current_time < total_cpu_time:
+            for i in range(num_processes):
+                if arrival_time[i] == current_time:
+                    output_file.write("Time "+str(current_time).rjust(3)+" : "+str(process_names[i]).rjust(2)+" arrived\n")
+                    process_queue.append(i)
+
+            # Manually created
+            if notRunning == False:
+                if finish_time[current_process] == current_time:
+                    output_file.write("Time "+str(finish_time[current_process]).rjust(3)+" : "+str(process_names[current_process]).rjust(2)+" finished\n")
+                    notRunning = True
+
+            # ChatGPT created but manually reformatted and rearranged
+            if len(process_queue) > 0:
+                if  notRunning == True: 
+                    current_process = process_queue.pop(0)
+                    start_time[current_process] = current_time
+                    output_file.write("Time "+str(start_time[current_process]).rjust(3)+" : "+str(process_names[current_process]).rjust(2)+" selected (burst "+str(burst_time[current_process]).rjust(3)+")\n")
+                    finish_time[current_process] = start_time[current_process] + burst_time[current_process]
+
+                    wait_time[current_process], turnaround_time[current_process], response_time[current_process] = metrics(arrival_time[current_process], start_time[current_process], finish_time[current_process])
+                    notRunning = False
+
+            # ChatGPT created but manually reformatted and rearranged with a little editing
+            if len(process_queue)==0:
+                if  notRunning == True:
+                    output_file.write("Time "+str(current_time).rjust(3)+" : Idle\n")
+
+            
+
+            current_time += 1
+                
+        output_file.write(f"Finished at time  {total_cpu_time}\n\n")        
+       
+
+        # ChatGPT created but manually reformatted
+        for i in range(num_processes):
+            output_file.write(str(process_names[i]).rjust(2)+" wait "+str(wait_time[i]).rjust(3)+" turnaround "+str(turnaround_time[i]).rjust(3)+" response "+str(response_time[i]).rjust(3)+"\n")
+
+            
+        # Manually created
+        for i in range(num_processes):
+            if finish_time[i] == 0 or finish_time[i] >= total_cpu_time:
+                output_file.write(f"{process_names[i]} did not finish\n")  
+
+
+# ChatGPT created with a little editing
+def metrics(arrival_time, start_time, finish_time):
+    wait_time = start_time - arrival_time
+    turnaround_time = finish_time - arrival_time
+    response_time = start_time - arrival_time
+    return wait_time, turnaround_time, response_time
+
+
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: python scheduler-gpt.py <inputfile.in>")
@@ -57,10 +135,24 @@ def main():
 
         data = parse_file(file_name)
         json_data = json.dumps(data, indent=4)
-        print(json_data)
+        #print(json_data)
     except ValueError as e:
         print(e)
         sys.exit(1)
+
+    
+    process_names = []
+    arrival_times = []
+    burst_times = []
+
+    for process in data["processes"]:
+        process_names.append(process["name"])
+        arrival_times.append(process["arrival"])
+        burst_times.append(process["burst"])
+
+    if data["algorithm"]=="fcfs":
+        fifo_scheduler(data["processcount"], process_names, data["runfor"], arrival_times, burst_times)
+    
 
 if __name__ == "__main__":
     main()
