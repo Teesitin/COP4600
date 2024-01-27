d4c9d3b9c87da3e6fb303f2518594a590b1b151b
aterry5 <123323801+aterry5@users.noreply.github.com>
2024-01-27 09:17:20 -0500

    Added HTML Output

--- PA1/scheduler-gpt.py
+++ PA1/scheduler-gpt.py
@@ -58,23 +58,32 @@ def fifo_scheduler(num_processes, process_names, total_cpu_time, arrival_time, b
     notRunning = True
     current_process = 0
 
+    # All html was edited ChatGPT code 
+    html_file = open("output.html","w")
+    html_file.write("<html><body>")
+
     with open("output.txt", "w") as output_file:
      
         # Manually created
         output_file.write(str(num_processes).rjust(3)+" processes\n")
         output_file.write("Using First-Come First-Served\n")
 
+        html_file.write("<p><font color= 'magenta'>"+str(num_processes).rjust(3)+" processes"+"</font></p>")
+        html_file.write("<p><font color= 'teal'>"+"Using First-Come First-Served"+"</font></p>")
+
         # ChatGPT created but manually reformatted and rearranged
         while current_time < total_cpu_time:
             for i in range(num_processes):
                 if arrival_time[i] == current_time:
                     output_file.write("Time "+str(current_time).rjust(3)+" : "+str(process_names[i]).rjust(2)+" arrived\n")
+                    html_file.write("<p><font color= 'blue'>"+"Time "+str(current_time).rjust(3)+" : "+str(process_names[i]).rjust(2)+" arrived"+"</font></p>")
                     process_queue.append(i)
 
             # Manually created
             if notRunning == False:
                 if finish_time[current_process] == current_time:
                     output_file.write("Time "+str(finish_time[current_process]).rjust(3)+" : "+str(process_names[current_process]).rjust(2)+" finished\n")
+                    html_file.write("<p><font color= 'maroon'>"+"Time "+str(finish_time[current_process]).rjust(3)+" : "+str(process_names[current_process]).rjust(2)+" finished"+"</font></p>")
                     notRunning = True
 
             # ChatGPT created but manually reformatted and rearranged
@@ -83,6 +92,7 @@ def fifo_scheduler(num_processes, process_names, total_cpu_time, arrival_time, b
                     current_process = process_queue.pop(0)
                     start_time[current_process] = current_time
                     output_file.write("Time "+str(start_time[current_process]).rjust(3)+" : "+str(process_names[current_process]).rjust(2)+" selected (burst "+str(burst_time[current_process]).rjust(3)+")\n")
+                    html_file.write("<p><font color= 'green'>"+"Time "+str(start_time[current_process]).rjust(3)+" : "+str(process_names[current_process]).rjust(2)+" selected (burst "+str(burst_time[current_process]).rjust(3)+")"+"</font></p>")
                     finish_time[current_process] = start_time[current_process] + burst_time[current_process]
 
                     wait_time[current_process], turnaround_time[current_process], response_time[current_process] = metrics(arrival_time[current_process], start_time[current_process], finish_time[current_process])
@@ -92,24 +102,32 @@ def fifo_scheduler(num_processes, process_names, total_cpu_time, arrival_time, b
             if len(process_queue)==0:
                 if  notRunning == True:
                     output_file.write("Time "+str(current_time).rjust(3)+" : Idle\n")
+                    html_file.write("<p><font color= 'navy'>"+"Time "+str(current_time).rjust(3)+" : Idle"+"</font></p>")
 
             
 
             current_time += 1
-                
-        output_file.write(f"Finished at time  {total_cpu_time}\n\n")        
+
+              
+        output_file.write(f"Finished at time  {total_cpu_time}\n\n") 
+        html_file.write("<p><font color= 'orangered'>"+"Finished at time  "+str(total_cpu_time)+"<br><br>"+"</font></p>")
+           
        
 
         # ChatGPT created but manually reformatted
         for i in range(num_processes):
             output_file.write(str(process_names[i]).rjust(2)+" wait "+str(wait_time[i]).rjust(3)+" turnaround "+str(turnaround_time[i]).rjust(3)+" response "+str(response_time[i]).rjust(3)+"\n")
+            html_file.write("<p><font color= 'deeppink'>"+str(process_names[i]).rjust(2)+" wait "+str(wait_time[i]).rjust(3)+" turnaround "+str(turnaround_time[i]).rjust(3)+" response "+str(response_time[i]).rjust(3)+"</font></p>")
 
             
         # Manually created
         for i in range(num_processes):
             if finish_time[i] == 0 or finish_time[i] >= total_cpu_time:
                 output_file.write(f"{process_names[i]} did not finish\n")  
+                html_file.write("<p><font color= 'crimson'>"+process_names[i]+" did not finish"+"</font></p>")
 
+        html_file.write("</body></html>")
+        html_file.close()
 
 # ChatGPT created with a little editing
 def metrics(arrival_time, start_time, finish_time):
